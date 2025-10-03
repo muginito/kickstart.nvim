d0e61a9ecee717b14153f08c57f82a84cebabf4e
muginito <miguel.vramos04@gmail.com>
2025-10-02 23:55:58 -0300

    add spelllang pt_br en_us

--- lua/options.lua
+++ lua/options.lua
@@ -66,12 +66,14 @@ vim.o.scrolloff = 10
 
 -- Indentation
 vim.o.tabstop = 4
+vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.o.autoindent = true
 vim.o.smartindent = true
 
--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.o.confirm = true
+
+-- Spell Checker langs
+vim.o.spelllang = 'pt_br,en_us'
